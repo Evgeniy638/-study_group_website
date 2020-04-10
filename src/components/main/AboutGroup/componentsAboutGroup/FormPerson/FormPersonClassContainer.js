1521b276e773366640b0e5da3f16246f4aee7232
Evgenya <subbotinjon01@mail.ru>
2020-04-11 00:52:42 +0300

    fix bag in Form About Group

--- src/components/main/AboutGroup/componentsAboutGroup/FormPerson/FormPersonClassContainer.js
+++ src/components/main/AboutGroup/componentsAboutGroup/FormPerson/FormPersonClassContainer.js
@@ -3,29 +3,29 @@ import PersonReduxForm from './FormPerson'
 
 export default class FormPersonClassContainer extends React.Component {
      handle = (data) => {
-          // if (data.image === undefined || data.image.length === 0) {
-          //      this.props.disablePersonForm()
-          //      this.props.createPerson({
-          //           surname: data.surname,
-          //           name: data.name,
-          //           patronymic: data.patronymic,
-          //           image: null
-          //      })
-          // } else {
-          //      const reader = new FileReader(data.image)
+          if (data.image === undefined || data.image.length === 0) {
+               this.props.disablePersonForm()
+               this.props.createPerson({
+                    surname: data.surname,
+                    name: data.name,
+                    patronymic: data.patronymic,
+                    image: null
+               })
+          } else {
+               const reader = new FileReader(data.image)
 
-          //      reader.onload = () => {
-          //           this.props.disablePersonForm()
-          //           this.props.createPerson({
-          //                surname: data.surname,
-          //                name: data.name,
-          //                patronymic: data.patronymic,
-          //                image: reader.result
-          //           })
-          //      }
+               reader.onload = () => {
+                    this.props.disablePersonForm()
+                    this.props.createPerson({
+                         surname: data.surname,
+                         name: data.name,
+                         patronymic: data.patronymic,
+                         image: reader.result
+                    })
+               }
 
-          //      reader.readAsDataURL(data.image[0])
-          // }
+               reader.readAsDataURL(data.image[0])
+          }
      }
 
      render() {
